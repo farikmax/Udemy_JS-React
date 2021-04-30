41ec1af4531a4991d00292c08f6fb82646fd1f3d
farikmax <farikmaxx@gmail.com>
2021-04-30 20:24:46 +0300

    feat: Lesson-retrieving elements from a page

---
+++ 17. Lesson-retrieving elements from a page/js/script.js
@@ -0,0 +1,22 @@
+//Получение элементов со страницы
+"use strict";
+
+const box = document.getElementById("box");
+console.log(box);
+
+// const btns = document.getElementsByTagName("button")[1];
+// console.log(btns);
+const btns = document.getElementsByTagName("button");
+console.log(btns[1]);
+
+const circles = document.getElementsByClassName("circle");
+console.log(circles);
+
+const hearts = document.querySelectorAll(".heart");
+console.log(hearts);
+hearts.forEach((item) => {
+  console.log(item);
+});
+
+const oneHeart = document.querySelector(".heart");
+console.log(oneHeart);
