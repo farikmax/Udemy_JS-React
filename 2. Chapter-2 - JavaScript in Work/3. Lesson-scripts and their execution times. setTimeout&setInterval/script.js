703ffc36d669621424b6bd5df2c70bf870fab5e0
farikmax <farikmaxx@gmail.com>
2021-05-13 19:27:34 +0300

    feat: add Lesson-scripts and their execution times. setTimeout&setInterval

---
+++ 2. Chapter-2 - JavaScript in Work/3. Lesson-scripts and their execution times. setTimeout&setInterval/script.js
@@ -0,0 +1,61 @@
+"use strict";
+// const timerId = setTimeout(
+// 	(text) => {
+// 		console.log(text);
+// 	},
+// 	2000,
+// 	"Hello"
+// );
+// -------------------------------------------------------------------
+// const timerId = setTimeout(logger, 2000);
+// clearInterval(timerId);
+
+// function logger() {
+// 	console.log("text");
+// }
+
+// -------------------------------------------------------------------
+// const btn = document.querySelector(".btn");
+// let timerId,
+// 	i = 0;
+
+// btn.addEventListener("click", () => {
+// 	// const timerId = setTimeout(logger, 2000);
+// 	timerId = setInterval(logger, 2000);
+// });
+
+// function logger() {
+// 	if (i === 3) {
+// 		clearInterval(timerId);
+// 	}
+// 	console.log("text");
+// 	i++;
+// }
+// -------------------------------------------------------------------рекурсивный метод вызова setTimeout
+
+// let id = setTimeout(function log() {
+// 	console.log("hello");
+// 	id = setTimeout(log, 500);
+// }, 500);
+// -------------------------------------------------------------------
+const btn = document.querySelector(".btn");
+let timerId,
+	i = 0;
+
+function myAnimation() {
+	const elem = document.querySelector(".box");
+	let pos = 0;
+
+	const id = setInterval(frame, 10);
+	function frame() {
+		if (pos == 300) {
+			clearInterval(id);
+		} else {
+			pos++;
+			elem.style.top = pos + "px";
+			elem.style.left = pos + "px";
+		}
+	}
+}
+
+btn.addEventListener("click", myAnimation);
