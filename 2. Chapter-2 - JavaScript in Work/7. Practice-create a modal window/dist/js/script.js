b6b36a14779e7ec682b84db1ae0bfb7455ff9b5c
farikmax <farikmaxx@gmail.com>
2021-05-18 10:21:24 +0300

    feat: add Practice-create a modal window (p.2)

--- 2. Chapter-2 - JavaScript in Work/7. Practice-create a modal window/dist/js/script.js
+++ 2. Chapter-2 - JavaScript in Work/7. Practice-create a modal window/dist/js/script.js
@@ -186,15 +186,30 @@ window.addEventListener("DOMContentLoaded", function () {
   const modalTrigger = document.querySelectorAll("[data-modal]"),
         modal = document.querySelector(".modal"),
         modalCloseBtn = document.querySelector("[data-close]");
-  modalTrigger.addEventListener("click", () => {
-    modal.classList.add("show");
-    modal.classList.remove("hide");
-    document.body.style.overflow = "hidden";
+  modalTrigger.forEach(btn => {
+    btn.addEventListener("click", () => {
+      modal.classList.add("show");
+      modal.classList.remove("hide");
+      document.body.style.overflow = "hidden";
+    });
   });
-  modalCloseBtn.addEventListener("click", () => {
+
+  function closeModal() {
     modal.classList.add("hide");
     modal.classList.remove("show");
     document.body.style.overflow = "";
+  }
+
+  modalCloseBtn.addEventListener("click", closeModal);
+  modal.addEventListener("click", e => {
+    if (e.target === modal) {
+      closeModal();
+    }
+  });
+  document.addEventListener("keydown", e => {
+    if (e.code === "Escape" && modal.classList.contains("show")) {
+      closeModal();
+    }
   }); // --------------------------------------------------------------------------------toggle
   // const modalTrigger = document.querySelectorAll("[data-modal]"),
   // 	modal = document.querySelector(".modal"),
